c0c42fd9d73070adc2052cae3ac315d3a905c82a
Chintapalli Durga Prasad <prasadchinthapalli51@gmail.com>
2026-08-08 10:18:18 +0530

    Add file upload route to database routes

--- server/src/routes/database.routes.js
+++ server/src/routes/database.routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { body } = require("express-validator");
 const validate = require("../middleware/validate");
 const { protect } = require("../middleware/auth.middleware");
+const upload = require("../middleware/upload.middleware");
 const controller = require("../controllers/database.controller");
 
 const router = express.Router();
 
 router.use(protect);
+
+router.post("/upload", upload.single("file"), controller.uploadFile);
 
 router.post(
   "/",
